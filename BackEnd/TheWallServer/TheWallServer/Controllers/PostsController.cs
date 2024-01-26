d6517955f9af5ec12a7d4cfd78fd617467cbed18
iamAndreait <andrealuna99@gmail.com>
2024-01-26 18:09:01 +0100

    Added PostsController

---
+++ BackEnd/TheWallServer/TheWallServer/Controllers/PostsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheWallServer.Data;
+using TheWallServer.Models;
+
+namespace TheWallServer.Controllers
+{
+    [ApiController]
+    [Route("posts")]
+    public class PostsController : ControllerBase
+    {
+        private readonly WallContext _context;
+
+        public PostsController(WallContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllPosts()
+        {
+            IQueryable<Post> posts = _context.Posts;
+            return Ok(posts.ToArray());
+        }
+    }
+}
--- BackEnd/TheWallServer/TheWallServer/Models/Post.cs
+++ BackEnd/TheWallServer/TheWallServer/Models/Post.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
+        public int UserId { get; set; }
         public User User { get; set; } = null!;
         public DateTime Date { get; set; }
     }
